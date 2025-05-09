3a0b3d7e635b3d6c8158f1f20a395ed7ef0670fe
soKaradag <so.karadag@gmail.com>
2025-05-09 13:09:44 +0300

    Created Register Response

--- src/main/java/com/hayvantakip/hayvantakip/models/User.java
+++ src/main/java/com/hayvantakip/hayvantakip/models/User.java
@@ -14,6 +14,7 @@ public class User {
     private String name;
     private String lastName;
     private String password;
+    private String email;
     private String phone;
     private String address;
 
@@ -21,11 +22,12 @@ public class User {
 
     }
 
-    public User(String name, String lastName, String password, String phone,
+    public User(String name, String lastName, String password,String email, String phone,
     String address){
         this.name = name;
         this.lastName = lastName;
         this.password = password;
+        this.email = email;
         this.phone = phone;
         this.address = address;
     }
@@ -60,6 +62,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getAddress() {
